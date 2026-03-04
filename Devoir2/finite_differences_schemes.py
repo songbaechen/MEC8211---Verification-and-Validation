886b1cf4127eea4eeca165540f899d6a37286890
Rawi <rawihlayhel@hotmail.com>
2026-03-04 07:25:16 -0500

    Modified file for unsteady problem implementation

--- Devoir2/finite_differences_schemes.py
+++ Devoir2/finite_differences_schemes.py
@@ -1,11 +1,11 @@
 """
-Schémas numériques d'ordre 1 et 2 pour la diffusion radiale.
+Schémas numériques d'ordre 2 pour la diffusion radiale.
 """
 import numpy as np
 
 from mesh_and_parameters import ProblemParameters, create_mesh
 
-def solve_scheme_2(param: ProblemParameters, n_profile: int) -> tuple[np.ndarray, np.ndarray]:
+def solve_unsteady_scheme(param: ProblemParameters, n_profile: int):
     """
     Fonction calculant la concentration du profil avec un schéma d'ordre 2.
     Retourne le maillage et un array contenant les concentrations aux nœuds résolu numériquement.
@@ -17,32 +17,54 @@ def solve_scheme_2(param: ProblemParameters, n_profile: int) -> tuple[np.ndarray
     """
     r_mesh, dr = create_mesh(param.r, n_profile)
 
-    a = np.zeros((n_profile, n_profile), dtype=np.float64)  # LHS/influence matrix
-    b = np.zeros(n_profile, dtype=np.float64)                     # RHS
+    D = param.d_eff
+    k = param.k
+    dt = param.dt
+    t_final = param.t_final
 
-    # symétrie, pas de flux à r=0
-    #a[0, 0] = 1.0
-    #a[0, 1] = -1.0
-    #b[0]    = 0.0
+    Nt = int(np.ceil(t_final / dt)) + 1
+    t = np.linspace(0.0, dt*(Nt-1), Nt)
 
-    a[0, 0] = -3.0
-    a[0, 1] =  4.0
-    a[0, 2] = -1.0
-    b[0] = 0.0
+    # C(r,0) = 0
+    Cn = np.zeros(n_profile, dtype = np.float64)
 
-    for i in range(1, n_profile-1):
-        ri = r_mesh[i]
+    C_hist = np.zeros((Nt, n_profile), dtype=np.float64)
+    C_hist[0, :] = Cn.copy()
 
-        a[i, i-1]   = (1.0 / dr**2) - (1.0 / (2.0 * dr * ri))
-        a[i, i]     = -2.0 / dr**2
-        a[i, i + 1] = (1.0 / dr**2) + (1.0 / (2.0 * dr * ri))
+    for n in range(Nt - 1): 
 
-        b[i] = param.s / param.d_eff
+        a = np.zeros((n_profile, n_profile), dtype=np.float64)  
+        b = np.zeros(n_profile, dtype=np.float64)                    
 
-    # condition de Dirichlet
-    a[n_profile - 1, :]             = 0.0
-    a[n_profile - 1, n_profile - 1] = 1.0
-    b[n_profile - 1]                = param.c_e
+        # Boundary conditions au centre
+        a[0, 0] = -3.0
+        a[0, 1] =  4.0
+        a[0, 2] = -1.0
+        b[0] = 0.0
 
-    c_array = np.linalg.solve(a.astype(np.float64), b.astype(np.float64))
-    return r_mesh, c_array
+        # Noeuds internes
+        for i in range(1, n_profile -1): 
+            
+            ri = r_mesh[i]
+
+            L_i_moins_1 = (1.0 / dr**2) - (1.0 / (2.0 * dr * ri))
+            L_i = -2.0 / dr**2
+            L_i_plus_1 = (1.0 / dr**2) + (1.0 / (2.0 * dr * ri))
+
+            a[i, i-1] = -D * L_i_moins_1
+            a[i, i] = (1.0 / dt + k) - D * L_i
+            a[i, i + 1] = -D * L_i_plus_1
+
+            b[i] = (1.0 / dt) * Cn[i]
+
+        # condition de Dirichlet au bord
+        a[n_profile - 1, :]             = 0.0
+        a[n_profile - 1, n_profile - 1] = 1.0
+        b[n_profile - 1]                = param.c_e
+
+        Cn_plus_1 = np.linalg(a, b)
+
+        C_hist[n+1, :] = Cn_plus_1
+        Cn = Cn_plus_1
+
+    return r_mesh, t , C_hist
