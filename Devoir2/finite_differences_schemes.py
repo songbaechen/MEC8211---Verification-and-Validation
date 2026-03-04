8b3d8c58f8b56659b0e02607aa7201033e62a152
Rawi <rawihlayhel@hotmail.com>
2026-03-04 09:11:43 -0500

    solver adapted for presence of MMS

--- Devoir2/finite_differences_schemes.py
+++ Devoir2/finite_differences_schemes.py
@@ -4,8 +4,9 @@ Schémas numériques d'ordre 2 pour la diffusion radiale.
 import numpy as np
 
 from mesh_and_parameters import ProblemParameters, create_mesh
+from mms_solution import MMSParams, mms_function, dirichlet_bord_mms, source_term_MMS
 
-def solve_unsteady_scheme(param: ProblemParameters, n_profile: int):
+def solve_unsteady_scheme(param: ProblemParameters, n_profile: int, mms: MMSParams | None = None):
     """
     Fonction calculant la concentration du profil avec un schéma d'ordre 2.
     Retourne le maillage et un array contenant les concentrations aux nœuds résolu numériquement.
@@ -17,22 +18,29 @@ def solve_unsteady_scheme(param: ProblemParameters, n_profile: int):
     """
     r_mesh, dr = create_mesh(param.r, n_profile)
 
-    D = param.d_eff
-    k = param.k
-    dt = param.dt
-    t_final = param.t_final
+    D = float(param.d_eff)
+    k = float(param.k)
+    dt = float(param.dt)
+    t_final = float(param.t_final)
 
     Nt = int(np.ceil(t_final / dt)) + 1
     t = np.linspace(0.0, dt*(Nt-1), Nt)
 
     # C(r,0) = 0
-    Cn = np.zeros(n_profile, dtype = np.float64)
+    if mms is None:
+        Cn = np.zeros(n_profile, dtype=np.float64)
+    else:
+        Cn = np.array(
+        [mms_function(float(r_i), 0.0, float(param.r), mms) for r_i in r_mesh],
+        dtype=np.float64
+    )
 
     C_hist = np.zeros((Nt, n_profile), dtype=np.float64)
     C_hist[0, :] = Cn.copy()
 
     for n in range(Nt - 1): 
 
+        t_np1 = float(t[n + 1])
         a = np.zeros((n_profile, n_profile), dtype=np.float64)  
         b = np.zeros(n_profile, dtype=np.float64)                    
 
@@ -57,10 +65,24 @@ def solve_unsteady_scheme(param: ProblemParameters, n_profile: int):
 
             b[i] = (1.0 / dt) * Cn[i]
 
+            # Source MMS
+            if mms is not None:
+                b[i] += source_term_MMS(
+                    ri,
+                    t_np1,
+                    float(param.r),
+                    D,
+                    k,
+                    mms,
+                )
+
         # condition de Dirichlet au bord
         a[n_profile - 1, :]             = 0.0
         a[n_profile - 1, n_profile - 1] = 1.0
-        b[n_profile - 1]                = param.c_e
+        if mms is None:
+            b[n_profile - 1] = float(param.c_e)
+        else:
+            b[n_profile - 1] = dirichlet_bord_mms(t_np1, float(param.r), mms)
 
         Cn_plus_1 = np.linalg.solve(a, b)
 
